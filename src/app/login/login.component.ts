47e2c3b7f9df138907cfb6b057495fa976727b7d
Svetoslav Yordanov <svetoslav.yordanov.003@gmail.com>
2022-09-11 12:21:04 +0200

    atuhentication

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -6,10 +8,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+loginForm: FormGroup;
+  constructor(private fb: FormBuilder, private authService: AuthService) {
+this.loginForm = this.fb.group({
+'username':['', [Validators.required]],
+'password':['', Validators.required]
+})
 
-  constructor() { }
+  }
 
   ngOnInit(): void {
   }
+login(){
+  this.authService.login(this.loginForm.value).subscribe(data =>{console.log(data)})
+}
 
+get username(){
+  return this.loginForm.get('username')
+}
+
+get password(){
+  return this.loginForm.get('password')
+}
 }
