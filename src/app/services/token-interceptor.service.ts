5d9774beef0b63b30dabb1f97a3d414bb1181686
yordanov03 <svetoslav.yordanov.003@gmail.com>
2022-09-14 11:36:14 +0200

    created interceptor for auth token (#3)

---
+++ src/app/services/token-interceptor.service.ts
@@ -0,0 +1,21 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { AuthService } from './auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TokenInterceptorService implements HttpInterceptor {
+
+  constructor(public authService: AuthService) { }
+  intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    request = request.clone({
+      setHeaders:{
+        Authorization: `Bearer ${this.authService.getToken()}`
+      }
+    })
+    return next.handle(request);
+  }
+}
